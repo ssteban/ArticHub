3d9b4f7c4506695063ec6112a06ed1708850e104
ssteban <142465683+ssteban@users.noreply.github.com>
2024-09-26 14:08:50 -0500

    Add files via upload

---
+++ frontend/js/inicio.js
@@ -0,0 +1,8 @@
+document.getElementById("menu_boton").addEventListener("click", function(){
+    let estilo=document.getElementById("menu").style.display;
+    if (estilo=="none"){
+        document.getElementById("menu").style.display="block";
+    }else{
+        document.getElementById("menu").style.display="none";
+    }
+})
