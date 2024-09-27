302fe28791ed0c4fc557d8b8f022b46e4b78b543
ssteban <142465683+ssteban@users.noreply.github.com>
2024-09-27 13:22:40 -0500

    Add files via upload

--- frontend/js/inicio.js
+++ frontend/js/inicio.js
@@ -5,5 +5,13 @@ document.getElementById("menu_boton").addEventListener("click", function(){
     }else{
         document.getElementById("menu").style.display="none";
     }
-})
+});
 
+document.getElementById("foto").addEventListener("click", function(){
+    let estilo=document.getElementById("perfil").style.display;
+    if (estilo=="none"){
+        document.getElementById("perfil").style.display="block";
+    }else{
+        document.getElementById("perfil").style.display="none";
+    }
+});
